4a5e3759c388cf2b5d1644980eed95b0fda78fc9
Paolo Destefanis <s257424@studenti.polito.it>
2020-04-18 21:31:05 +0200

    layout item details fragment

---
+++ app/src/main/java/it/polito/mad/team19lab2/ItemDetailsFragment.kt
@@ -0,0 +1,32 @@
+package it.polito.mad.team19lab2
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ItemDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ItemDetailsFragment()
+    }
+
+    private lateinit var viewModel: ItemDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.item_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ItemDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/it/polito/mad/team19lab2/ItemDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package it.polito.mad.team19lab2
+
+import androidx.lifecycle.ViewModel
+
+class ItemDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
