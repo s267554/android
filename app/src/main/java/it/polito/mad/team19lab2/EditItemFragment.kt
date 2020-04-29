122702ffb6c9b3d98b9d5e2ade90f8ff3608c9a7
vittorio ferraresi <s267557@studenti.polito.it>
2020-04-29 22:10:18 +0200

    toast2

--- app/src/main/java/it/polito/mad/team19lab2/EditItemFragment.kt
+++ app/src/main/java/it/polito/mad/team19lab2/EditItemFragment.kt
@@ -317,12 +317,12 @@ class EditItemFragment : Fragment() {
             commit()
         }
         if(arguments?.getBoolean("deep_link")==true){
-            val msg = "Item created: ${titleEditText.text.toString()}"
+            val msg = "Item created"
             Toast.makeText(context,msg, Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_nav_edit_item_to_nav_home, bundleOf("item_id1" to id_item))
         }
         else {
-            val msg = "Item updated: ${titleEditText.text.toString()}"
+            val msg = "Item updated"
             Toast.makeText(context,msg, Toast.LENGTH_SHORT).show()
             findNavController().navigate(
                 R.id.action_nav_edit_item_to_nav_item_detail,
