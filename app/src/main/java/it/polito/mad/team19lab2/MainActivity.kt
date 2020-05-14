a3f2699e707f906114b8198da6f08e5f12dd8248
Paolo Destefanis <s257424@studenti.polito.it>
2020-05-14 23:59:33 +0200

    user id and navitagion to edit profile

--- app/src/main/java/it/polito/mad/team19lab2/MainActivity.kt
+++ app/src/main/java/it/polito/mad/team19lab2/MainActivity.kt
@@ -77,8 +77,7 @@ class MainActivity : AppCompatActivity() {
             userVm.getOrCreateUser().observe(this, Observer{
                 nav_view.getHeaderView(0).findViewById<TextView>(R.id.header_title_textView).text = it.fullname
                 nav_view.getHeaderView(0).findViewById<TextView>(R.id.header_subtitle_textView).text = it.nickname
-                if(it.fullname.isNullOrEmpty() || it.nickname.isNullOrEmpty())
-                    navController.navigate(R.id.action_nav_home_to_nav_edit_profile)
+
                 //download image
             })
         }
@@ -102,8 +101,6 @@ class MainActivity : AppCompatActivity() {
                 userVm.getOrCreateUser().observe(this, Observer {
                     nav_view.getHeaderView(0).findViewById<TextView>(R.id.header_title_textView).text = it.fullname
                     nav_view.getHeaderView(0).findViewById<TextView>(R.id.header_subtitle_textView).text = it.nickname
-                    if(it.fullname.isNullOrEmpty() || it.nickname.isNullOrEmpty())
-                        navController.navigate(R.id.action_nav_home_to_nav_edit_profile)
                     // download image
                 })
             } else {
