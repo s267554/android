74e7e23c2429b1eca3935beb28b43f62a9d15099
Filippo Fontan s267554 <s267554@studenti.polito.it>
2020-05-22 09:59:40 +0200

    Already deployed Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -31,6 +31,7 @@ exports.notifyOwner = functions.firestore
 
       return db.collection('items').doc(context.params.itemId).get().then(item => {
           const owner = item.data().userId
+          const title = item.data().title
           console.log('ownerId is', owner);
           return db.collection('utenti').doc(owner).get().then(doc => {
             const token = doc.data().notificationToken
@@ -38,8 +39,8 @@ exports.notifyOwner = functions.firestore
             // Notification details.
             const payload = {
               notification: {
-                title: 'Someone got interested!',
-                body: `${fullname} has expressed interest`
+                title: 'Your item got some love!',
+                body: `${fullname} has expressed interest in ${title}`
               },
               token: token
             };
@@ -55,65 +56,40 @@ exports.notifyOwner = functions.firestore
       });
     });
 
-exports.sendFollowerNotification = functions.database.ref('/followers/{followedUid}/{followerUid}')
-    .onWrite(async (change, context) => {
-      const followerUid = context.params.followerUid;
-      const followedUid = context.params.followedUid;
-      // If un-follow we exit the function.
-      if (!change.after.val()) {
-        return console.log('User ', followerUid, 'un-followed user', followedUid);
-      }
-      console.log('We have a new follower UID:', followerUid, 'for user:', followedUid);
+exports.sendFollowerNotification = functions.firestore.document('items/{itemId}')
+    .onUpdate((change, context) => {
+        const newState = change.after.data().state
+        const oldState = change.before.data().state
+        const title = change.after.data().title
 
-      // Get the list of device notification tokens.
-      const getDeviceTokensPromise = admin.database()
-          .ref(`/users/${followedUid}/notificationTokens`).once('value');
+        if(newState == oldState){return;}
 
-      // Get the follower profile.
-      const getFollowerProfilePromise = admin.auth().getUser(followerUid);
+        const promises = []
+        const tokens = []
 
-      // The snapshot to the user's tokens.
-      let tokensSnapshot;
-
-      // The array containing all the user's tokens.
-      let tokens;
-
-      const results = await Promise.all([getDeviceTokensPromise, getFollowerProfilePromise]);
-      tokensSnapshot = results[0];
-      const follower = results[1];
-
-      // Check if there are any device tokens.
-      if (!tokensSnapshot.hasChildren()) {
-        return console.log('There are no notification tokens to send to.');
-      }
-      console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-      console.log('Fetched follower profile', follower);
-
-      // Notification details.
-      const payload = {
-        notification: {
-          title: 'You have a new follower!',
-          body: `${follower.displayName} is now following you.`,
-          icon: follower.photoURL
-        }
-      };
-
-      // Listing all tokens as an array.
-      tokens = Object.keys(tokensSnapshot.val());
-      // Send notifications to all tokens.
-      const response = await admin.messaging().sendToDevice(tokens, payload);
-      // For each message check if there was an error.
-      const tokensToRemove = [];
-      response.results.forEach((result, index) => {
-        const error = result.error;
-        if (error) {
-          console.error('Failure sending notification to', tokens[index], error);
-          // Cleanup the tokens who are not registered anymore.
-          if (error.code === 'messaging/invalid-registration-token' ||
-              error.code === 'messaging/registration-token-not-registered') {
-            tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-          }
-        }
-      });
-      return Promise.all(tokensToRemove);
+        return db.collection('items').doc(context.params.itemId).collection('users').get().then( async (col) => {
+            col.forEach(async (item) => {
+                promises.push(
+                    db.collection('utenti').doc(item.data().id).get().then( async (utente) => {
+                          return tokens.push(utente.data().notificationToken);
+                      })
+                    );
+            })
+            await Promise.all(promises);
+            console.log("Tokens:", tokens);
+            const payload = {
+              notification: {
+                title: 'Update on an item you were following',
+                body: `${title} is now ${newState}!`
+              }
+            };
+            return admin.messaging().sendToDevice(tokens, payload).then((response) => {
+                console.log("Message sent successfully:", response);
+                return response;
+            })
+            .catch((error) => {
+                console.log("Error sending message: ", error, payload);
+            });
+        });
     });
+
